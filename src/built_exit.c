53119cb69df05d52bbac51acf57e4b9bf058ff95
nimai <119419194+inozo22@users.noreply.github.com>
2023-05-23 15:45:01 +0200

    add built_exit

--- src/built_exit.c
+++ src/built_exit.c
@@ -6,19 +6,43 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 13:05:08 by nimai             #+#    #+#             */
-/*   Updated: 2023/05/23 14:14:11 by nimai            ###   ########.fr       */
+/*   Updated: 2023/05/23 15:43:32 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+/**
+ * @brief check if it's numeric.
+ * @note ignore the space before the number begins. also fine with boolean
+ */
+int	is_numeric(char *num)
+{
+	while (*num == 32)
+		num++;
+	while (*num)
+	{
+		if (!ft_isdigit(*num))
+			return (0);
+		num++;
+	}	
+	return (1);
+}
+
+/**
+ * @brief doesn't leave from the program, just return.
+ * @note maybe it's not necessary to have as a function. 
+ */
 int	error_exit_msg(int ret, char *msg)
 {
-	ft_printf(msg);
-
+	ft_printf("exit\n%s\n", msg);
 	return (ret);
 }
 
+/**
+ * @brief to know argv's amount.
+ * @return how many argvs you have in int. 
+ */
 int	av_amount(char **strs)
 {
 	int	ret;
@@ -31,22 +55,31 @@ int	av_amount(char **strs)
 	return (ret);
 }
 
+/**
+ * @brief manage "builtin" exit cmd.
+ * @return if there are more than cmd + 1 argument, return to minishell prompt, without execute any function after.
+ */
 int	built_exit(char **av)
 {
 	int	amount;
 
 	amount = av_amount(av);
-	printf("amount is %d\n", amount);
+	printf("Line: %d\n", __LINE__);
 	if (amount == 2)
-		exit(0);
-	else if (amount > 3)
+		exit (0);
+	if (!is_numeric(av[2]))
 	{
-		return (error_exit_msg(1, "minishell: exit: too many arguments\n"));
+		//give you error msg, but exit works. (as working bash)
+		ft_printf("minishell: exit: %s: numeric argument required\n", av[2]);
+	//will eliminate
+		ft_printf("but EXIT!\n");
+		exit (1);
 	}
-	else
+	if (amount > 3)
 	{
-		
+		return (error_exit_msg(1, "minishell: exit: too many arguments"));
 	}
-
+	ft_printf("EXIT!\n");
+	exit (ft_atoi(av[2]));
 	return (0);
 }
