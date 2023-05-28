2fab565fa290aa6d51ed63a38aa23d586093c515
inozo22 <119419194+inozo22@users.noreply.github.com>
2023-05-28 09:34:44 +0200

    fix pwd

--- src/built_pwd.c
+++ src/built_pwd.c
@@ -6,7 +6,7 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/23 16:11:36 by nimai             #+#    #+#             */
-/*   Updated: 2023/05/23 16:51:39 by nimai            ###   ########.fr       */
+/*   Updated: 2023/05/28 09:33:57 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,6 +22,8 @@ int	built_pwd(char **av)
 {
 	(void)av;
 	ft_printf(getcwd(NULL, 0));
+	ft_printf("\n");
+	system("leaks minishell");
 	return (0);
 	//230523nimai: should be managed the flags? (at least give some message?)
 }
