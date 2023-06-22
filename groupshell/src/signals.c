562deca647b1f9b6d23806702194e9ab23c954a1
nimai <119419194+inozo22@users.noreply.github.com>
2023-06-22 09:59:11 +0200

    works ctrl + C more or less

--- groupshell/src/signals.c
+++ groupshell/src/signals.c
@@ -6,29 +6,27 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 14:11:50 by nimai             #+#    #+#             */
-/*   Updated: 2023/06/21 16:24:14 by nimai            ###   ########.fr       */
+/*   Updated: 2023/06/22 09:45:07 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
+#include <regex.h>
 
 /**
  * @brief when push Ctrl + D, exit 
- * 
- * 
  * @note as the super free after the while, only write "exit" and break the loop
  */
 void	sig_eof(void)
 {
 	write(STDOUT_FILENO, "exit\n", 5);
-//	exit (0);
 }
 
 void	action(int sig)
 {
 	(void)sig;
 	rl_on_new_line();
-	write(1, "\n", 1);
+	write(1, "\t\t\t\b\b\b\b\b   \n", 12);
 	rl_replace_line("", 0);
 	rl_redisplay();
 }
