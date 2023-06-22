767ebef8e68600db16ea858c351d2867852d0dc2
nimai <119419194+inozo22@users.noreply.github.com>
2023-06-22 10:31:13 +0200

    #16 it's working with dirty way. There are possibility to make it better

--- groupshell/src/signals.c
+++ groupshell/src/signals.c
@@ -6,7 +6,7 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/29 14:11:50 by nimai             #+#    #+#             */
-/*   Updated: 2023/06/22 09:45:07 by nimai            ###   ########.fr       */
+/*   Updated: 2023/06/22 10:26:24 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,15 @@ void	sig_eof(void)
 
 void	action(int sig)
 {
+	char	*str;
+	char	*str2;
+
+	str = "\t\t\t\b\b\b\b\b                                                        ";
+	str2 = "                             \n";
 	(void)sig;
 	rl_on_new_line();
-	write(1, "\t\t\t\b\b\b\b\b   \n", 12);
+	write(1, str, 54);
+	write(1, str2, 30);
 	rl_replace_line("", 0);
 	rl_redisplay();
 }
