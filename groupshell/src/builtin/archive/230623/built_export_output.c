1af98ec649449cdbdf517b5fddf7aa7bce9945fd
nimai <119419194+inozo22@users.noreply.github.com>
2023-06-23 16:41:24 +0200

    .

--- groupshell/src/builtin/archive/230623/built_export_output.c
+++ groupshell/src/builtin/archive/230623/built_export_output.c
@@ -6,7 +6,7 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 12:44:12 by nimai             #+#    #+#             */
-/*   Updated: 2023/06/23 16:40:42 by nimai            ###   ########.fr       */
+/*   Updated: 2023/06/23 16:38:19 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,6 +102,122 @@ t_export	*fill_list(char **env, t_export *ret)
 	}
 	return (ret);
 }
+
+/* t_export	*fill_list(char **strs, t_export *ret)
+{
+	int			i[3];
+	char		*tmp;
+
+	ft_bzero(i, 3 * sizeof(int));
+	while (strs[i[2]])
+		i[2]++;
+	ret->plen = i[2];
+	while (i[0] < ret->plen)
+	{
+		ret->box[i[0]].equal = 0;
+		ret->box[i[0]].id = i[0];
+		if (!strs[i[0]])
+			return (NULL);
+		i[1] = 0;
+		while (strs[i[0]][i[1]] != '=' && strs[i[0]][i[1]] != '\0')
+			i[1]++;
+		if (strs[i[0]][i[1]] == '=')
+			ret->box[i[0]].equal = 1;
+		ret->box[i[0]].name = ft_calloc(2000, sizeof(char));
+		ret->box[i[0]].val = ft_calloc(2000, sizeof(char));
+		if (!ret->box[i[0]].name || !ret->box[i[0]].val)
+			return (NULL);
+		ft_strlcpy(ret->box[i[0]].name, strs[i[0]], i[1] + 1);
+		tmp = ft_substr(strs[i[0]], i[1] + 1, ft_strlen(strs[i[0]]) - i[1]);
+		ft_strlcpy(ret->box[i[0]].val, tmp, ft_strlen(strs[i[0]]) - i[1] + 1);
+		free (tmp);
+		i[0]++;
+	}
+	return (ret);
+} */
+
+/**
+ * @brief initiation t_data
+ * @author nimai
+ * @param name it will include until "="
+ * @note It's not beautiful to allocate 2000 each box[i].name and val, but it's necessary to sort.
+ * i[0] = i
+ * i[1] = len
+ * i[2] = c
+ */
+/* t_export	*fill_list(char **strs, t_export *ret)
+{
+	int			i[3];
+	char		*tmp;
+
+	ft_bzero(i, 3 * sizeof(int));
+	while (strs[i[2]])
+		i[2]++;
+	ret->plen = i[2];
+	while (i[0] < ret->plen)
+	{
+		ret->box[i[0]].equal = 0;
+		ret->box[i[0]].id = i[0];
+		if (!strs[i[0]])
+			return (NULL);
+		i[1] = 0;
+		while (strs[i[0]][i[1]] != '=' && strs[i[0]][i[1]] != '\0')
+			i[1]++;
+		if (strs[i[0]][i[1]] == '=')
+			ret->box[i[0]].equal = 1;
+		ret->box[i[0]].name = ft_calloc(2000, sizeof(char));
+		ret->box[i[0]].val = ft_calloc(2000, sizeof(char));
+		if (!ret->box[i[0]].name || !ret->box[i[0]].val)
+			return (NULL);
+		ft_strlcpy(ret->box[i[0]].name, strs[i[0]], i[1] + 1);
+		tmp = ft_substr(strs[i[0]], i[1] + 1, ft_strlen(strs[i[0]]) - i[1]);
+		ft_strlcpy(ret->box[i[0]].val, tmp, ft_strlen(strs[i[0]]) - i[1] + 1);
+		free (tmp);
+		i[0]++;
+	}
+	return (ret);
+} */
+
+/**
+ * @brief initiation t_data
+ * @author nimai
+ * @param name it will include until "="
+ * @note It's not beautiful to allocate 2000 each box[i].name and val, but it's necessary to sort.
+ */
+/* t_export	*fill_list(char **strs, t_export *ret)
+{
+	int			i;
+	int			len;
+	char		*tmp;
+	int			c;
+
+	c = 0;
+	while (strs[c])
+		c++;
+	ret->plen = c;
+	i = -1;
+	while (++i < ret->plen)
+	{
+		ret->box[i].equal = 0;
+		ret->box[i].id = i;
+		if (!strs[i])
+			return (NULL);
+		len = 0;
+		while (strs[i][len] != '=' && strs[i][len] != '\0')
+			len++;
+		if (strs[i][len] == '=')
+			ret->box[i].equal = 1;
+		ret->box[i].name = ft_calloc(2000, sizeof(char));
+		ret->box[i].val = ft_calloc(2000, sizeof(char));
+		if (!ret->box[i].name || !ret->box[i].val)
+			return (NULL);
+		ft_strlcpy(ret->box[i].name, strs[i], len + 1);
+		tmp = ft_substr(strs[i], len + 1, ft_strlen(strs[i]) - len);
+		ft_strlcpy(ret->box[i].val, tmp, ft_strlen(strs[i]) - len + 1);
+		free (tmp);
+	}
+	return (ret);
+} */
 
 /**
  * @brief print export, until free t_export
