8d3d3428c4a05df674422ca6c0c7688bad23eb9a
nimai <119419194+inozo22@users.noreply.github.com>
2023-08-01 18:44:11 +0200

    .

--- test/test.c
+++ test/test.c
@@ -6,23 +6,24 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 12:22:31 by nimai             #+#    #+#             */
-/*   Updated: 2023/08/01 13:05:03 by nimai            ###   ########.fr       */
+/*   Updated: 2023/08/01 18:42:40 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "../lib/libft/libft.h"
 
-typedef struct s_list
+/* typedef struct s_list
 {
 	char			*content;
 	int				type;
 	int				cmd_pos;
 	struct s_list	*next;
-}			t_list;
+}			t_list; */
 
-size_t	ft_strlen(const char *str)
+/* size_t	ft_strlen(const char *str)
 {
 	size_t	i;
 
@@ -31,17 +32,19 @@ size_t	ft_strlen(const char *str)
 		i++;
 	return (i);
 }
-
+ */
 
 int	main(int ac, char **av)
 {
 	char	*str;
 	char	*str1;
 	char	*str2;
+	char	c;
 	int i = 0;
     t_list  list;
 
-	str = av[1];
+	c = av[1][0];
+	str = ft_strdup(av[1]);
 	str1 = av[2];
 	str2 = av[3];
 
@@ -49,12 +52,14 @@ int	main(int ac, char **av)
     // printf("Imhere");
     // if (!list->content)
     //     return (1);
-    list.content = av[1];
+    list.content = str;
+	printf("length: %lu\n", strlen(&c));
     printf("list->content: %p\n", list.content);
     printf("length: %lu\n", strlen(list.content));
     printf("length: %lu\n", ft_strlen(list.content));
 
-	printf("str: %s\n", str);
+	printf("c: %s\n", &c);
+	printf("str: %s\n", str + 2);
 	printf("str1: %s\n", str1);
 	printf("str2: %s\n", str2);
 
