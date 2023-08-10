74965775dfa2d45bcf04bec15e9d5defcd8ec1da
nimai <119419194+inozo22@users.noreply.github.com>
2023-08-10 14:04:29 +0200

    expanser...

--- nimishell/src/expanser2.c
+++ nimishell/src/expanser2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   expanser.c                                         :+:      :+:    :+:   */
+/*   expanser2.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bde-mada <bde-mada@student.42.fr>          +#+  +:+       +#+        */
+/*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/19 09:49:30 by bde-mada          #+#    #+#             */
-/*   Updated: 2023/06/19 09:49:30 by bde-mada         ###   ########.fr       */
+/*   Created: 2023/08/10 10:32:20 by nimai             #+#    #+#             */
+/*   Updated: 2023/08/10 10:32:20 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,22 @@
 #include "../lib/libft/libft.h"
 #include <stdio.h>
 
-char	*get_var_value(char *env_var, char *envp[])
+char	*get_var_value(char *env_var, char *envp[], int len)
 {
 	int	i;
-	int	len;
+//	int	len;
 
-	printf("\n\nenv var: %s\n", env_var);
+	printf("\n\nenv var: %s len: %d\n", env_var, len);
 	i = -1;
-	len = ft_strlen(env_var);
+//	len = ft_strlen(env_var) - 1;
 	while (envp[++i])
 	{
-		printf("pos of = : %c\n", envp[i][len]);
+		printf("pos of = : %c, envp[%d]: %s\n", envp[i][len], i, envp[i]);
 		if (!ft_strncmp(env_var + 1, envp[i], len) && envp[i][len] == '=')
+		{
+			printf("%sLine: %d HERE I AM%s\n", COLOR_RED, __LINE__, COLOR_RESET);
 			return (ft_strdup(envp[i] + len + 1));
+		}
 	}
 	return (ft_substr(env_var, 0, len));
 }
@@ -50,30 +53,59 @@ char	*get_var_value(char *env_var, char *envp[])
  * @note ($$) Expands to the pid of the current shell.
  * @note ($1-9) Expands to the corresponding arguments passed to bash
  */
-char	*is_expand(char *token, char *envp[], t_data *data)
+char	*is_expand(char *token, int len, char *envp[], t_data *data)
 {
 /* 	int	i;
 
 	i = -1; */
+	printf("%sHERE I AM%s\n", COLOR_RED, COLOR_RESET);
 	if (!ft_strncmp(token, "$?", 2))
+	{
+		printf("%sLine: %d HERE I AM%s\n", COLOR_RED, __LINE__, COLOR_RESET);
 		return (ft_itoa(data->return_val));
+	}
 	if (!ft_strncmp(token, "$!", 2))
+	{
+		printf("%sLine: %d HERE I AM%s\n", COLOR_RED, __LINE__, COLOR_RESET);
 		return (ft_itoa(data->return_val));
+	}
 	if (!ft_strncmp(token, "$$", 2))
+	{
+		printf("%sLine: %d HERE I AM%s\n", COLOR_RED, __LINE__, COLOR_RESET);
 		return (ft_itoa(data->pid));
+	}
 	if (!ft_strncmp(token, "$-", 2))
+	{
+		printf("%sLine: %d HERE I AM%s\n", COLOR_RED, __LINE__, COLOR_RESET);
 		return ("himBH");
+	}
 	if (!ft_strncmp(token, "$@", 2) || !ft_strncmp(token, "$*", 2))
+	{
+		printf("%sLine: %d HERE I AM%s\n", COLOR_RED, __LINE__, COLOR_RESET);
 		return ("");
+	}
 	if (!ft_strncmp(token, "$#", 2))
+	{
+		printf("%sLine: %d HERE I AM%s\n", COLOR_RED, __LINE__, COLOR_RESET);
 		return ("0");
+	}
 	if (!ft_strncmp(token, "$IFS", 4))
+	{
+		printf("%sLine: %d HERE I AM%s\n", COLOR_RED, __LINE__, COLOR_RESET);
 		return ("\t\n");
-	if (!ft_isdigit(token[1]))
+	}
+	if (ft_isdigit(token[1]))
+	{
+		printf("%sLine: %d HERE I AM%s\n", COLOR_RED, __LINE__, COLOR_RESET);
 		return ("");
+	}
 	if (!(token[1]) || (!ft_isalnum(token[1]) && token[1] != '_'))
+	{
+		printf("%sLine: %d HERE I AM%s\n", COLOR_RED, __LINE__, COLOR_RESET);
 		return (NULL);
-	return(get_var_value(token, envp));
+	}
+	printf("%sLine: %d HERE I AM%s\n", COLOR_RED, __LINE__, COLOR_RESET);
+	return(get_var_value(token, envp, len));
 }
 
 /**
@@ -124,50 +156,138 @@ char	*expand(char *arg, int start, int end, char *var_value, char *envp[])
 // 	return (expanded);
 // }
 
+
 char	*expanser(char *arg, char *envp[], t_data *data)
 {
 	char	*expanded;
-	char	*var_value;
+//	char	*var_value;
 	char	*tmp;
 	//int		i;
 	char 	*pos[2];
 
 	//i = 0;//changed from -1
 	expanded = ft_strdup(arg);
-	pos[0] = ft_strchr(expanded, '$');
+	pos[0] = ft_strdup(expanded);
+	pos[0] = ft_strchr(pos[0], '$');
 	//if there is no "$", never enter
-	while (pos[0] && arg[0] != '\'')
+	while (pos[0] && arg[0] != '\'' && ft_strlen(pos[0]))
 	{
-		printf("%sI will expand: %s, pos[0]: %s%s\n", COLOR_RED, expanded, pos[0], COLOR_RESET);
+		printf("%sexpanser len: %zu, pos[0]: %s%s\n", COLOR_RED, ft_strlen(pos[0]), pos[0], COLOR_RESET);
 		pos[1] = pos[0] + 1;
-		while (ft_isalnum(*pos[1]))
+		while (ft_isalnum(*pos[1])/*  || *pos[1] == '$' */)
 			pos[1]++;
-		printf("start: %s, end: %s\n", pos[0], pos[1]);
-		tmp = expanded;
-		printf("tmp: %s expanded: %s\n", tmp, expanded);
-//		free (expanded);
-		var_value = is_expand(pos[0], envp, data);//check unique letter
-		printf("var value: %s\n", var_value);
-		if (!var_value)
+		// if (*pos[1] == '$')
+		// 	pos[1]++;
+		printf("pos[1]: %s\n", pos[1]);
+		tmp = is_expand(pos[0], pos[1] - pos[0]- 1, envp, data);
+		printf("%sAfter expand tmp: %s pos[1] - pos[0]: %ld%s\n", COLOR_RED, tmp, pos[1] - pos[0], COLOR_RESET);
+		if (ft_strcmp(expanded, arg) != 0)
+			expanded = ft_strjoin(expanded, tmp);
+		else
 		{
-			var_value = expand(expanded, pos[0] - expanded, pos[1] - pos[0], var_value, envp);
+			printf("%sLine: %d HERE I AM%s\n", COLOR_RED, __LINE__, COLOR_RESET);
+			free (expanded);
+			expanded = ft_calloc(ft_strlen(tmp), 1);
+			ft_strcpy(expanded, tmp);
 		}
-		expanded = (char *)ft_calloc(ft_strlen(var_value) + ft_strlen(tmp) + 1, 1);
-		ft_strcpy(expanded, tmp);
-		ft_strcat(expanded, var_value);
-	//	printf("i val: %d, i pos after write: %s\n",i, arg + i);
-		if (pos[0][0] == '$')
-			free(var_value);
-		free (tmp);
-		pos[0] += pos[1] - pos[0] + 1;
-		printf("%spos[0]: %s%s\n", COLOR_RED, pos[0], COLOR_RESET);
+		free(tmp);
+		printf("expanded: %s\n", expanded);
+		//pos[0] += pos[1] - pos[0] + 1;
+		pos[0] = pos[1];
+		// if (!ft_isalnum(pos[1][1]))
+		// 	pos[0]++;
+		printf("After move pos[0]: %s, len: %zu\n", pos[0], ft_strlen(pos[0]));
+		// if (!ft_strlen(pos[0]))
+		// 	break ;
 	}
 	/**
 	 * @note int i is not necesarry
 	 */
 	return (expanded);
 }
-//230809nimai: expanded should be whole string for the arg
+// 		printf("pos[0]: %d\n", *pos[0]);
+// 		printf("%sI will expand: %s, pos[0]: %s%s\n", COLOR_RED, expanded, pos[0], COLOR_RESET);
+// 		pos[1] = pos[0] + 1;
+// 		while (ft_isalnum(*pos[1]))
+// 			pos[1]++;
+// 		printf("start: %s, end: %s\n", pos[0], pos[1]);
+// 		tmp = ft_strdup(expanded);
+// 		printf("Line: %d, tmp: %s expanded: %s\n", __LINE__, tmp, expanded);
+// 		free (expanded);
+// 		var_value = is_expand(pos[0], envp, data);//check unique letter
+// 		while (*var_value)
+// 		{
+// 			printf("%c", *var_value);
+// 			var_value++;
+// 		}
+// 		write(1, "\n", 1);
+// //		printf("var value: %s, var value[0]: %d\n", var_value, var_value[0]);
+// 		if (!var_value)
+// 		{
+// 			var_value = expand(expanded, pos[0] - expanded, pos[1] - pos[0], var_value, envp);
+// 		}
+// 		expanded = (char *)ft_calloc(ft_strlen(var_value) + ft_strlen(tmp) + 1, 1);
+// 		ft_strcpy(expanded, tmp);
+// 		ft_strcat(expanded, var_value);
+// 		printf("Line: %d, tmp: %s expanded: %s\n", __LINE__, tmp, expanded);
+// 	//	printf("i val: %d, i pos after write: %s\n",i, arg + i);
+// 		if (pos[0][0] == '$')
+// 			free(var_value);
+// 		free (tmp);
+// 		pos[0] += pos[1] - pos[0] + 1;
+// 		printf("%spos[0]: %s%s\n", COLOR_RED, pos[0], COLOR_RESET);
+
+// char	*expanser(char *arg, char *envp[], t_data *data)
+// {
+// 	char	*expanded;
+// 	char	*var_value;
+// 	char	*tmp;
+// 	//int		i;
+// 	char 	*pos[2];
+
+// 	//i = 0;//changed from -1
+// 	expanded = ft_strdup(arg);
+// 	pos[0] = ft_strchr(expanded, '$');
+// 	//if there is no "$", never enter
+// 	while (pos[0] && arg[0] != '\'')
+// 	{
+// 		printf("%sI will expand: %s, pos[0]: %s%s\n", COLOR_RED, expanded, pos[0], COLOR_RESET);
+// 		pos[1] = pos[0] + 1;
+// 		while (ft_isalnum(*pos[1]))
+// 			pos[1]++;
+// 		printf("start: %s, end: %s\n", pos[0], pos[1]);
+// 		tmp = ft_strdup(expanded);
+// 		printf("Line: %d, tmp: %s expanded: %s\n", __LINE__, tmp, expanded);
+// 		free (expanded);
+// 		var_value = is_expand(pos[0], envp, data);//check unique letter
+// 		while (*var_value)
+// 		{
+// 			printf("%c", *var_value);
+// 			var_value++;
+// 		}
+// 		write(1, "\n", 1);
+// //		printf("var value: %s, var value[0]: %d\n", var_value, var_value[0]);
+// 		if (!var_value)
+// 		{
+// 			var_value = expand(expanded, pos[0] - expanded, pos[1] - pos[0], var_value, envp);
+// 		}
+// 		expanded = (char *)ft_calloc(ft_strlen(var_value) + ft_strlen(tmp) + 1, 1);
+// 		ft_strcpy(expanded, tmp);
+// 		ft_strcat(expanded, var_value);
+// 		printf("Line: %d, tmp: %s expanded: %s\n", __LINE__, tmp, expanded);
+// 	//	printf("i val: %d, i pos after write: %s\n",i, arg + i);
+// 		if (pos[0][0] == '$')
+// 			free(var_value);
+// 		free (tmp);
+// 		pos[0] += pos[1] - pos[0] + 1;
+// 		printf("%spos[0]: %s%s\n", COLOR_RED, pos[0], COLOR_RESET);
+// 	}
+// 	/**
+// 	 * @note int i is not necesarry
+// 	 */
+// 	return (expanded);
+// }
+//230810nimai: expanded should be the whole string for the parm arg
 
 /* int	main(int argc, char *argv[], char *envp[])
 {
--- nimishell/src/minishell.c
+++ nimishell/src/minishell.c
@@ -6,12 +6,11 @@
 /*   By: nimai <nimai@student.42urduliz.com>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 09:32:33 by bde-mada          #+#    #+#             */
-/*   Updated: 2023/08/09 16:33:14 by nimai            ###   ########.fr       */
+/*   Updated: 2023/08/10 10:38:59 by nimai            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-#include <termios.h>
 
 /**
  * @note 230804nimai: at the moment, when there is no environment valur, user name will be "noname"
@@ -159,14 +158,14 @@ int	minishell(t_data *data)
 	char			*prompt;
 
 // these are remove ^C in the prompt
-//	struct termios	termios_save;
-//	struct termios	term;
+	struct termios	termios_save;
+	struct termios	term;
 
-//	tcgetattr(0, &termios_save);
-//	tcsetattr(0, 0, &termios_save);
-//	term = termios_save;
-//	term.c_lflag &= ~ECHOCTL;
-//	tcsetattr(0, TCSASOFT, &term);
+	tcgetattr(0, &termios_save);
+	tcsetattr(0, 0, &termios_save);
+	term = termios_save;
+	term.c_lflag &= ~ECHOCTL;
+	tcsetattr(0, TCSASOFT, &term);
 // these are remove ^C in the prompt
 
 	printf("pid in minishell: %d\n", data->pid);
